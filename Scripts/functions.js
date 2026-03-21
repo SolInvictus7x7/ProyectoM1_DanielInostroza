c14aa72c313a6b3b1ad5163228f89043a024975f
SolInvictus7x7 <dan.inosnun@gmail.com>
2026-03-20 23:37:19 -0300

    hechas funciones básicas, cambios de lógica en HTML

--- Scripts/functions.js
+++ Scripts/functions.js
@@ -1,8 +1,10 @@
 function tipoColor() {
     const checkbox = document.querySelector(".tipo-color");
     let colorSeleccionado = checkbox.checked;
+    return colorSeleccionado
 }
 
-const checkbox = document.querySelector(".tipo-color");
-let colorSeleccionado = checkbox.checked;
-console.log(colorSeleccionado)
+function cantidadColor() {
+    const cantidadSelect = document.querySelector('input[name="outputs"]:checked');
+    return cantidadSelect ? cantidadSelect.value : null;
+}
--- Scripts/main.js
+++ Scripts/main.js
@@ -1,1 +1,3 @@
-import * as funciones from "./functions"
+
+
+document.getElementById('paleta').remove();
