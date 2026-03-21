8fcff4066a0dc6932d1b989c26d466b364c0d19e
SolInvictus7x7 <dan.inosnun@gmail.com>
2026-03-20 23:06:12 -0300

    prueba

---
+++ Scripts/functions.js
@@ -0,0 +1,8 @@
+function tipoColor() {
+    const checkbox = document.querySelector(".tipo-color");
+    let colorSeleccionado = checkbox.checked;
+}
+
+const checkbox = document.querySelector(".tipo-color");
+let colorSeleccionado = checkbox.checked;
+console.log(colorSeleccionado)
---
+++ Scripts/main.js
@@ -0,0 +1,1 @@
+import * as funciones from "./functions"
