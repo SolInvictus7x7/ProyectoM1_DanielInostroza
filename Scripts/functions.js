618d66fb56934a6fe433f67d1ae9ac7be63913f9
SolInvictus7x7 <dan.inosnun@gmail.com>
2026-03-24 22:03:22 -0300

    codigos colores visibles en paletas

--- Scripts/functions.js
+++ Scripts/functions.js
@@ -1,14 +1,9 @@
-export function tipoColor() {
-    const checkbox = document.querySelector(".tipo-color");
-    let colorSeleccionado = checkbox.checked;
-    return colorSeleccionado
-}
-
 export function generar() {
     const template = document.querySelector('.paleta-temp');
     const target = document.querySelector('article');
     const clonar = template.content.cloneNode(true);
     const cantidad = document.querySelector('input[name="outputs"]:checked').value;
+    const tipoColor = document.querySelector('.tipo-color').checked;
 
     if (cantidad === "6") {
         clonar.querySelectorAll('#c2, #c3').forEach(el => el.remove());
@@ -16,5 +11,14 @@ export function generar() {
         clonar.querySelectorAll('#c3').forEach(el => el.remove());
     }
 
-    target.appendChild(clonar);
+    clonar.querySelectorAll('.color').forEach(li => {
+        const color = tipoColor ? 
+            `hsl(${Math.floor(Math.random() * 360)}, ${Math.floor(Math.random() * 100)}%, ${Math.floor(Math.random() * 100)}%)` : 
+            `rgba(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, 1)`;
+        
+        li.style.backgroundColor = color;
+        li.textContent = color; 
+    });
+
+    target.prepend(clonar);
 }
--- Scripts/main.js
+++ Scripts/main.js
@@ -4,4 +4,3 @@ import * as funciones from "./functions.js";
 const botonGenerar = document.querySelector(".boton-generar");
 botonGenerar.addEventListener('click', funciones.generar);
 
-//====Cantidad Colores====
